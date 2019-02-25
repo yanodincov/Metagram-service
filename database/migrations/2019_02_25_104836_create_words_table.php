3dedf30404ba867b65993fe1d0b45fc97aa7cb4a
Yan Odincov <yanodincov@gmail.com>
2019-02-25 14:09:59 +0300

    Remove .idea folder. Add Word model and migration. Add dictionaries ("storage/app/dict/") on storage folder.

--- database/migrations/2019_02_25_104836_create_words_table.php
+++ database/migrations/2019_02_25_104836_create_words_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateWordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('words', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->increments('id');
+            $table->string('word', 4);
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('words');
     }
 }
